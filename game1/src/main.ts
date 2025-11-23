863ef080130ec90775060d1520c8f5ebd4e2d689
Omicronian <36453867+Omicronian@users.noreply.github.com>
2025-11-23 11:13:55 -0800

    Create main.ts

---
+++ game1/src/main.ts
@@ -0,0 +1,19 @@
+import Phaser from 'phaser';
+import GameScene from './scenes/GameScene';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  scene: [GameScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  }
+};
+
+const game = new Phaser.Game(config);
